2c06b1f71cbb6902101c1dfc3405b3e57d51295e
Troy Nguyen <55474315+troynguyeen@users.noreply.github.com>
2026-01-14 10:06:15 +0700

    Add tags to aws_codeconnections_connection resource

--- demo-codeconnections/main.tf
+++ demo-codeconnections/main.tf
@@ -1,4 +1,7 @@
 resource "aws_codeconnections_connection" "demo-githubconnection" {
   name          = var.name
   provider_type = var.provider_type
+  tags = {
+      Name = "Custom tag name"
+  }
 }
