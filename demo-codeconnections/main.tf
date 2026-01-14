d4f35b18d5cb05b53e21ad19615c58a5cd49c7c3
Troy Nguyen <55474315+troynguyeen@users.noreply.github.com>
2026-01-14 10:09:39 +0700

    Fix formatting in main.tf for AWS CodeConnections

--- demo-codeconnections/main.tf
+++ demo-codeconnections/main.tf
@@ -2,6 +2,6 @@ resource "aws_codeconnections_connection" "demo-githubconnection" {
   name          = var.name
   provider_type = var.provider_type
   tags = {
-      Name = "Custom tag name"
+    Name = "Custom tag name"
   }
 }
