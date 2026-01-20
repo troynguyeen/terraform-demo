9603fb6962407c6b27758fb720a3ce37e3378393
Troy Nguyen <55474315+troynguyeen@users.noreply.github.com>
2026-01-20 15:32:00 +0700

    Update IAM permission networking

--- demo-codebuild/iam.tf
+++ demo-codebuild/iam.tf
@@ -63,6 +63,8 @@ locals {
           "ec2:DescribeSubnets",
           "ec2:DescribeSecurityGroups",
           "ec2:DescribeVpcs",
+          "ec2:DescribeVpcAttribute",
+          "ec2:DescribeDhcpOptions"
         ]
         resources = ["*"]
       }
